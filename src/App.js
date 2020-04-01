495fcb883e6294681ed84c0fd9b638bdea902178
Amit Cohen <amicohen@corp.devalore.com>
2020-04-01 22:38:37 +0300

    create FS app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import PlayerForm from './pages/PlayerForm.js'
 
 function App() {
-  
+  const [state,setState] = useState({})
+  const getAPI = ()=>{
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => setState({ apiResponse: res }));
+  }
+
+  useEffect(() => {
+    getAPI();
+    
+  }, [])
   return (
     <div className="App">
     <PlayerForm/>
+    <h1>{state.apiResponse}</h1>
     </div>
+    
   );
 }
 
